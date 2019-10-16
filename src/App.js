d69559381eae50ce896a7d669638c0f378e00dc5
Sharan Sajiv Menon <sharan@Sharan-MBP.fios-router.home>
2019-10-16 17:27:14 -0400

    Pushing to github

--- src/App.js
+++ src/App.js
@@ -1,26 +1,125 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Paper, Typography, TextField, Button, Checkbox, Divider } from '@material-ui/core';
+import { List, ListItem, ListItemText, ListItemSecondaryAction, IconButton } from '@material-ui/core';
+import { Delete } from '@material-ui/icons';
+import { withStyles } from '@material-ui/core/styles'
+import Dialog from "./dialog"
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const styles = {
+  root: {
+    margin: "0 auto",
+    marginTop: 100,
+    padding: 20,
+    maxWidth: 400,
+    marginBottom: 50
+  },
+  form: {
+    display: "flex",
+    flexDirection: 'row',
+    alignItems: "baseline",
+    justifyContent: 'space-evenly'
+  },
+  list: {
+    border: "1px solid gray"
+  }
 }
-
-export default App;
+export class App extends React.Component {
+  constructor(props) {
+    super(props);
+    let list = JSON.parse(localStorage.getItem("list"))
+    if (!list) {
+      localStorage.setItem("list", JSON.stringify([]))
+    }
+    this.state = {
+      items: list ? list : [],
+      title: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleCreate = this.handleCreate.bind(this);
+    this.handleCheck = this.handleCheck.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+  handleChange({ target: { name, value } }) {
+    this.setState({
+      [name]: value
+    })
+  }
+  handleCreate(e) {
+    e.preventDefault();
+    if (this.state.title) {
+      this.setState((prevState) => {
+        let Items = {
+          title: prevState.title,
+          id: Date.now(),
+          checked: false
+        }
+        let items = [...prevState.items, Items];
+        localStorage.setItem("list", JSON.stringify(items))
+        return {
+          items,
+          title: ""
+        }
+      })
+    }
+  }
+  handleCheck(id) {
+    this.setState(({ items }) => {
+      let item = items.find(ex => ex.id === id)
+      item.checked = !item.checked
+      localStorage.setItem("list", JSON.stringify(items))
+      return { items: items }
+    })
+  }
+  handleDelete(id) {
+    this.setState(({ items }) => {
+      let item = items.filter(ex => ex.id !== id)
+      localStorage.setItem("list", JSON.stringify(item))
+      return { items: item }
+    })
+  }
+  render() {
+    const { classes } = this.props
+    return (
+      <Paper className={classes.root}>
+        <Typography variant="h4" align="center" gutterBottom>
+          Items
+        </Typography>
+        <form onSubmit={this.handleCreate} className={classes.form}>
+          <TextField
+            name='title'
+            label='Item'
+            value={this.state.title}
+            onChange={this.handleChange}
+            margin='normal'
+            autoComplete="off"
+          />
+          <Button type='submit' color='primary' variant="contained">
+            Create Item
+          </Button>
+        </form>
+        <List>
+          {this.state.items.map(({ id, title, checked }) => (
+            <ListItem key={id}>
+              <Checkbox
+                edge="start"
+                checked={checked}
+                onClick={() => this.handleCheck(id)}
+                tabIndex={-1}
+              />
+              <ListItemText primary={title} />
+              <ListItemSecondaryAction>
+                <IconButton color='primary' onClick={() => this.handleDelete(id)}>
+                  <Delete />
+                </IconButton>
+              </ListItemSecondaryAction>
+            </ListItem>
+          )
+          )}
+        </List>
+        {/* <Divider /> */}
+        <Dialog />
+      </Paper>
+    )
+  }
+}
+export default withStyles(styles)(App)
---
+++ src/dialog.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Grow from '@material-ui/core/Grow';
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+    return <Grow ref={ref} {...props} />;
+});
+
+export default function AlertDialogSlide() {
+    const [open, setOpen] = React.useState(false);
+
+    function handleClickOpen() {
+        setOpen(true);
+    }
+
+    function handleClose() {
+        setOpen(false);
+    }
+
+    return (
+        <div>
+            <Button variant="outlined" color="primary" onClick={handleClickOpen}>
+                Learn More
+            </Button>
+            <Dialog
+                open={open}
+                TransitionComponent={Transition}
+                keepMounted
+                onClose={handleClose}
+                aria-labelledby="alert-dialog-slide-title"
+                aria-describedby="alert-dialog-slide-description"
+            >
+                <DialogTitle id="alert-dialog-slide-title">{"A Simple Todo App"}</DialogTitle>
+                <DialogContent dividers>
+                    <DialogContentText id="alert-dialog-slide-description">
+                        This app was created by Sharan Sajiv Menon to test Material UI Out.
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose} color="primary">
+                        Close
+                    </Button>
+                </DialogActions>
+            </Dialog>
+        </div>
+    );
+}
